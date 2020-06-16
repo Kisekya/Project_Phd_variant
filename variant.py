fcf2208b0be2da0dacd02017d0956ebc3cdf38dc
Kisekya <coralie.muller.mazin@gmail.com>
2020-06-16 09:37:35 +0200

    request function and test for the parse file

--- variant.py
+++ variant.py
@@ -7,7 +7,17 @@
 import sys
 import argparse
 import requests
+from requests import ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError
+import urllib.response
 import json
+# import fileinput
+# import re
+import pandas as pd
+
+
+#Test
+# chr9:g.140695360G>A
+#chr16:g.28883241A>G
 
 #######################
 ##    Functions      ##
@@ -15,16 +25,96 @@ import json
 #Function for check the format and print the help if its not a right format
 def check_format(file):
     if file.lower().endswith(('.txt')):
+        # parse_file(file)
         print("txt")
-        #f = open('Variants', 'r')
     else:
         print("Sorry, your input is not found. Please refer to the help.")
         parser.print_help() #print the help link to the parser
-        
 
 
 
+def request_variant():
+    url='http://myvariant.info/v1/variant/'
+    variant='chr16:g.28883241A>G'
+    url=url+variant
+    print(url)
+    parameters= 'fields=snpeff'
+    for i in range(2):
+        try:
+            r = requests.get(url,params=parameters,timeout=6.0)
+            print(r.url)
+        except requests.HTTPError as e:
+            print("Sorry!! Connection Error. \n")
+            print(str(e))  
+        except requests.ConnectionError as e:
+            print("Sorry!! Connection Error. Make sure you are connected to Internet. Technical Details given below.\n")
+            print(str(e))            
+            continue
+        except requests.Timeout as e:
+            print("Sorry!! Timeout Error")
+            print(str(e))
+            continue
+        except requests.RequestException as e:
+            print("Sorry!! General Error")
+            print(str(e))
+            continue
+        except KeyboardInterrupt:
+            print("Someone closed the program")
+    json_data=r.json()
+    num=0
+    try:
+        variable=json_data['snpeff']['ann']['putative_impact']
+        if variable=="HIGH":
+            num=1
+            print("work")
+        # add_annotation(json_data, num)
+    except:
+        for i in json_data['snpeff']['ann']:
+            if i['putative_impact']== "MODERATE":
+                num=2
+            else:
+                continue
+        if num==2:
+        # add_annotation(json_data, num)
+            print(2)
 
+# def add_annotation(json_data, num):
+#     if num ==1:
+#         result = {}
+#         for key in json_data.keys():
+#             if not isinstance(json_data[key], dict):
+#                 result[key] = json_data[key]
+#             else:
+#                 result.update(add_annotation(json_data[key]))
+#         return result
+#     elif num==2:
+#         print("nnnn")
+
+
+# json_datas = json.load()
+# json_list = [j[1][0] for j in json_datas.iterrows()]
+# parsed_list = [parse_nested_json(j) for j in json_list]
+# result = pd.DataFrame(parsed_list)
+# print (result)
+# # result.to_csv("my_csv_file.csv", index=False)
+    
+
+    
+def parse_file(file):
+    df = open('file.txt', "r")
+    lines = df.readlines()
+    df.close()
+    for index, line in enumerate(lines):
+      lines[index] = line.strip()
+    
+
+#     with open(file) as f:
+#         file=f.read().split("\n")
+#         print(file)
+#         i=0
+#         for line in file:
+#             print(file[i])
+#             i+=1 TO FINISH
 
 #######################
 ##      Main         ##
@@ -44,8 +134,8 @@ if __name__=='__main__':
     putative_impact=args.impact
     maf_value = args.value
     output= args.output
-
-     check_format(file)
+    request_variant()
+    check_format(file)
 
    
     
